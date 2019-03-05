75a76ae6bf3ff3768fdc2ad060a05941848c5786
matscus <matscus@yandex.ru>
2019-03-05 23:24:41 +0300

    new metod

--- mqjms/ProducerImpl.go
+++ mqjms/ProducerImpl.go
@@ -16,7 +16,7 @@ import (
 	"log"
 	"strconv"
 )
-
+var putMsgHandle ibmmq.MQMessageHandle
 // ProducerImpl defines a struct that contains the necessary objects for
 // sending messages to a queue on an IBM MQ queue manager.
 type ProducerImpl struct {
@@ -70,16 +70,7 @@ func (producer ProducerImpl) Send(dest jms20subset.Destination, msg jms20subset.
 		// Configure the put message options, including asking MQ to allocate a
 		// unique message ID
 		pmo.Options = ibmmq.MQPMO_NO_SYNCPOINT | ibmmq.MQPMO_NEW_MSG_ID 
-		var putMsgHandle ibmmq.MQMessageHandle
-		smpo := ibmmq.NewMQSMPO()
-		pd := ibmmq.NewMQPD()
-		k:="test"
-		v:="test ok"
-			err = putMsgHandle.SetMP(smpo, k, pd, v)
-			if err != nil {
-				fmt.Printf("PROP1: %v\n", err)
-			}
-		pmo.OriginalMsgHandle=putMsgHandle//getStringPropetry(property)
+		pmo.OriginalMsgHandle=getStringPropetry(property)
 		// Convert the JMS persistence into the equivalent MQ message descriptor
 		// attribute.
 		if producer.deliveryMode == jms20subset.DeliveryMode_NON_PERSISTENT {
@@ -206,7 +197,7 @@ func (producer *ProducerImpl) GetTimeToLive() int {
 // }
 func getStringPropetry(property map[string]string)ibmmq.MQMessageHandle{
 	var err error
-	var putMsgHandle ibmmq.MQMessageHandle
+	
 	smpo := ibmmq.NewMQSMPO()
 	pd := ibmmq.NewMQPD()
 	for k,v:= range property{

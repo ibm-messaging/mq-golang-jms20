b2af4bd9738ba037e19daa982d8cd99eb9c179b0
Matt Roberts <32040584+matrober-uk@users.noreply.github.com>
2021-02-12 16:12:53 +0000

    Hello world application
    (cherry picked from commit f2c76ad6214350c6ca93ea74ed6765c48613e32d)

---
+++ openshift-app-sample/src/main.go
@@ -0,0 +1,16 @@
+// Copyright (c) IBM Corporation 2021.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License 2.0, which is available at
+// http://www.eclipse.org/legal/epl-2.0.
+//
+// SPDX-License-Identifier: EPL-2.0
+
+// Package main provides the entry point for a executable application.
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!!!")
+}
